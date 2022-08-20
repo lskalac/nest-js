55a1ab571d526edfcf6cbc4dae2781c43a75c84a
Lorena Škalac <lorena.skalac@softwaresauna.com>
2022-08-20 12:05:36 +0200

    - definining and applying middlewares

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { ValidateCustomerMiddleware } from './middlewares/validate-customer.middleware';
 import { CustomersService } from './services/customers/customers.service';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService]
 })
-export class CustomersModule {}
+// configuring and applying middlewares:
+export class CustomersModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    // we can pass and a function here instead of class (so we can implement middleware as 
+    // a function too) but by nest is recommended class way
+    consumer.apply(ValidateCustomerMiddleware, (req: Request, res: Response, next: NextFunction) => {
+      console.log('I am second middleware');
+      next();
+    })
+    .forRoutes({
+      path: 'customers/:id',
+      method: RequestMethod.GET
+    });
+  }
+}
